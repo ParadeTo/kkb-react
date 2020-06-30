0f645dae3d7a349468e68531d48e2fe127d476d7
youxingzhi <xingzhi.you@shopee.com>
2020-06-30 10:09:24 +0800

    support use in class component

--- lesson1/src/components/my-rc-field-form/Field.js
+++ lesson1/src/components/my-rc-field-form/Field.js
@@ -1,37 +1,37 @@
-import React, {Component, cloneElement} from "react";
-import FieldContext from "./FieldContext";
+import React, {Component, cloneElement} from 'react'
+import FieldContext from './FieldContext'
 
 export default class Field extends Component {
-  static contextType = FieldContext;
+  static contextType = FieldContext
   componentDidMount() {
-    this.cancelRegister = this.context.registerField(this);
+    this.cancelRegister = this.context.registerField(this)
   }
 
   componentWillUnmount() {
     if (this.cancelRegister) {
-      this.cancelRegister();
+      this.cancelRegister()
     }
   }
 
   onStoreChange = () => {
-    this.forceUpdate();
-  };
+    this.forceUpdate()
+  }
   getControled = () => {
-    const {getFieldValue, setFieldsValue} = this.context;
-    const {name} = this.props;
+    const {getFieldValue, setFieldsValue} = this.context
+    const {name} = this.props
     return {
       value: getFieldValue(name), //从store中取值
-      onChange: e => {
+      onChange: (e) => {
         // 把新的参数值存到store中
-        const newValue = e.target.value;
-        setFieldsValue({[name]: newValue});
-        console.log("newValue", newValue); //sy-log
-      }
-    };
-  };
+        const newValue = e.target.value
+        setFieldsValue({[name]: newValue})
+        console.log('newValue', newValue) //sy-log
+      },
+    }
+  }
   render() {
-    const {children} = this.props;
-    const returnChildNode = React.cloneElement(children, this.getControled());
-    return returnChildNode;
+    const {children} = this.props
+    const returnChildNode = React.cloneElement(children, this.getControled())
+    return returnChildNode
   }
 }
--- lesson1/src/pages/MyRCFieldForm.js
+++ lesson1/src/pages/MyRCFieldForm.js
@@ -1,40 +1,75 @@
-import React, {Component, useEffect} from "react";
+import React, {Component, useEffect} from 'react'
 // import Form, {Field} from "rc-field-form";
-import Form, {Field} from "../components/my-rc-field-form/";
-import Input from "../components/Input";
+import Form, {Field} from '../components/my-rc-field-form/'
+import Input from '../components/Input'
 
-const nameRules = {required: true, message: "请输入姓名！"};
-const passworRules = {required: true, message: "请输入密码！"};
+const nameRules = {required: true, message: '请输入姓名！'}
+const passworRules = {required: true, message: '请输入密码！'}
 
-export default function MyRCFieldForm(props) {
-  const [form] = Form.useForm();
+// export default function MyRCFieldForm(props) {
+//   const [form] = Form.useForm()
 
-  const onFinish = val => {
-    console.log("onFinish", val); //sy-log
-  };
+//   const onFinish = (val) => {
+//     console.log('onFinish', val)
+//   }
 
-  // 表单校验失败执行
-  const onFinishFailed = val => {
-    console.log("onFinishFailed", val); //sy-log
-  };
+//   const onFinishFailed = (val) => {
+//     console.log('onFinishFailed', val)
+//   }
 
-  useEffect(() => {
-    console.log("form", form); //sy-log
-    //form.setFieldsValue({username: "default"});
-  }, []);
+//   useEffect(() => {
+//     console.log('form', form)
+//     form.setFieldsValue({username: 'default'})
+//     console.log(form.getFieldValue('username'))
+//   }, [])
 
-  return (
-    <div>
-      <h3>MyRCFieldForm</h3>
-      <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Field name="username" rules={[nameRules]}>
-          <Input placeholder="input UR Username" />
-        </Field>
-        <Field name="password" rules={[passworRules]}>
-          <Input placeholder="input UR Password" />
-        </Field>
-        <button>Submit</button>
-      </Form>
-    </div>
-  );
+//   return (
+//     <div>
+//       <h3>MyRCFieldForm</h3>
+//       <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+//         <Field name='username' rules={[nameRules]}>
+//           <Input placeholder='input UR Username' />
+//         </Field>
+//         <Field name='password' rules={[passworRules]}>
+//           <Input placeholder='input UR Password' />
+//         </Field>
+//         <button>Submit</button>
+//       </Form>
+//     </div>
+//   )
+// }
+export default class extends React.Component {
+  formRef = React.createRef()
+
+  onFinish = (val) => {
+    console.log('onFinish', val)
+  }
+
+  onFinishFailed = (val) => {
+    console.log('onFinishFailed', val)
+  }
+
+  componentDidMount() {
+    this.formRef.current.setFieldsValue({username: 'default'})
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>MyRCFieldForm</h3>
+        <Form
+          ref={this.formRef}
+          onFinish={this.onFinish}
+          onFinishFailed={this.onFinishFailed}>
+          <Field name='username' rules={[nameRules]}>
+            <Input placeholder='input UR Username' />
+          </Field>
+          <Field name='password' rules={[passworRules]}>
+            <Input placeholder='input UR Password' />
+          </Field>
+          <button>Submit</button>
+        </Form>
+      </div>
+    )
+  }
 }
